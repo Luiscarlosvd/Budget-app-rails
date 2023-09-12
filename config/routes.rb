72fc61a76336057966bdab0f4b512fb241a29508
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-12 18:10:30 -0400

    Add remove button for group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:index, :new, :create, :show], path: 'categories' do
+  resources :groups, only: [:index, :new, :create, :show, :destroy], path: 'categories' do
     resources :purchases, only: [:new, :create ], path: 'transactions'
   end
   root 'groups#index'
